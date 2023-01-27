a47fefbec543cc408322fa5865bf4cbef71acb3e
alebarrero <72764436+alebarrero@users.noreply.github.com>
2023-01-26 21:58:12 -0500

    Updated the movie API to display movie image & generate random movie

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '4cc24add61mshaefd388a3de842fp16df08jsnd17fb033b479',
-		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+// 	const body = document.body
+	// body.append("test")
+	
+	const movieImageDiv = document.getElementById("movieImage");
+	console.log(movieImageDiv)
+	function displayMovie(data){
+		const randomNumber= Math.floor(Math.random() * 49) 
+		const options = {
+			method: 'GET',
+			// params: {titleType: 'movie', genre: 'romance', endYear: '2023', startYear: '2015'},
+			headers: {
+			  'X-RapidAPI-Key': '4cc24add61mshaefd388a3de842fp16df08jsnd17fb033b479',
+			  'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+			}
+		  };
+		// const options = {
+		// 	method: 'GET',
+		// 	params:{titleType:"movie"},
+		// 	headers: {
+		// 		'X-RapidAPI-Key': '4cc24add61mshaefd388a3de842fp16df08jsnd17fb033b479',
+		// 		'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
+		// 	}
+		// };
+		
+		fetch('https://moviesdatabase.p.rapidapi.com/titles?titleType=movie&genre=Romance&endYear=2023&startYear=2021&limit=50',options)
+		
+			.then(response => response.json())
+			.then(response => {
+				console.log(response)
+				var img = document.createElement("img")
+				img.setAttribute("src",response.results[randomNumber].primaryImage.url )
+				const title = document.createElement("p")
+				title.textContent = response.results[randomNumber].titleText.text
+				const year = document.createElement("p")
+				year.textContent = response.results[randomNumber].titleText.text
+				const description = document.createElement("p")
+				description.textContent = response.results[randomNumber].titleText.text
+				movieImageDiv.append(img,title)
+			})
+			.catch(err => console.error(err));
 	}
-};
-
-fetch('https://moviesdatabase.p.rapidapi.com/titles?genre=Romance&limit=9&endYear=2023&startYear=2015', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+	displayMovie()
